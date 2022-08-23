4c0a8061d3fea698a9e56b8577a12468451e304e
Zakarias <46316857+zach721@users.noreply.github.com>
2022-08-23 23:15:06 +0200

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,46 @@
+#Python prime number script
+#Author: Zakarias
+
+def generate(num:int):
+    
+  list = []
+  for x in range(num):
+     x+=1
+     list.append(x)
+
+  list.pop(0)
+  list.pop(num-2)
+  mssg=""
+  for x in list:
+  
+    while num%x!=0:
+      mssg = str(num)+" is prime"
+
+      break
+      return mssg
+    else:
+       #mssg = str(num)+" isn't prime"
+       break
+       #return mssg
+  
+  print(mssg)
+  
+
+
+def printnumber(numb:int):
+  
+  print('1 is prime\n')
+
+  print('2 is prime\n')
+  
+  for x in range(numb):
+    if x == 0:
+        continue
+    if x == 1:
+        continue
+    if x == 2:
+        continue
+    generate(x)
+    
+
+printnumber(30)
